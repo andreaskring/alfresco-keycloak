5ee97868dcf617c9060b7b90785258e1c214eaef
Andreas Kring <andreas@magenta.dk>
2020-09-16 15:03:14 +0200

    Changed some constants

--- utils/rest-api-admin.py
+++ utils/rest-api-admin.py
@@ -1,17 +1,17 @@
 import json
 import requests
 
-REALM = 'hurra'
-CLIENT_SECRET = '58332228-267c-4c17-9801-265c16b26a96'
+REALM = 'alfresco'
+CLIENT_SECRET = '48cfa07e-adff-4a63-a17c-493f84546f2b'
 
 # NOT the same as client-id (Keycloak thing...)
 # E.g.
 # client-id = alfresco
 # "id of client" = e315ead7-c626-4b4e-9eff-21d0d529b1bc
-CUSTOM_ID_CLIENT = 'e315ead7-c626-4b4e-9eff-21d0d529b1bc'
+CUSTOM_ID_CLIENT = '6a3422ce-92e7-4d71-a705-116dc3e85bf3'
 
 
-token_url = f'http://localhost:8180/auth/realms/{REALM}' \
+token_url = f'http://localhost:8280/auth/realms/{REALM}' \
             f'/protocol/openid-connect/token'
 
 # Get token
@@ -32,7 +32,7 @@ headers = {
 
 # Get list of clients
 
-CLIENTS_URL = f'http://localhost:8180/auth/admin/realms/{REALM}/clients'
+CLIENTS_URL = f'http://localhost:8280/auth/admin/realms/{REALM}/clients'
 r = requests.get(CLIENTS_URL, headers=headers)
 print(r.status_code, r.url)
 print(json.dumps(r.json(), indent=2))
@@ -43,7 +43,7 @@ payload = {
     'enabled': False
 }
 
-CUSTOM_CLIENT_URL = f'http://localhost:8180/auth/admin/realms/{REALM}' \
+CUSTOM_CLIENT_URL = f'http://localhost:8280/auth/admin/realms/{REALM}' \
                     f'/clients/{CUSTOM_ID_CLIENT}'
 r = requests.put(CUSTOM_CLIENT_URL, json=payload, headers=headers)
 print(r.status_code, r.url)
